0fed01e008fb3e5e91d0e1ea74ff4e623d7ae600
Viacheslav Kuznetsov <alxim192@gmail.com>
2022-11-30 20:06:11 +0800

    fixing

--- src/main/java/jsonlUtils/JsonlFile.java
+++ src/main/java/jsonlUtils/JsonlFile.java
@@ -3,6 +3,8 @@ package jsonlUtils;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class JsonlFile {
     private String path = "results/";
@@ -13,6 +15,7 @@ public class JsonlFile {
 
     public void createFile() throws IOException {
         File file = new File(path);
+        Files.createDirectories(Path.of(path).getParent());
         if(file.exists()) file.delete();
         file.createNewFile();
     }
